742a4b39f90b9ae279928126e144ab816153a61f
kpregtech <kpregtech@gmail.com>
2026-02-03 16:30:53 +0530

    Fix ALLOWED_HOSTS and add CSRF settings

--- burgerverse_backend/burgerverse_backend/settings.py
+++ burgerverse_backend/burgerverse_backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = ['burgerverse.onrender.com']
 
 
 
