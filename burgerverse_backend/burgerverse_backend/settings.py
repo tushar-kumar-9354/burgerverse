2325a2b3b19a9fd71a4e8c563926b494dd1fa58a
kpregtech <kpregtech@gmail.com>
2026-02-03 16:37:42 +0530

    Fix ALLOWED_HOSTS and add CSRF settings

--- burgerverse_backend/burgerverse_backend/settings.py
+++ burgerverse_backend/burgerverse_backend/settings.py
@@ -139,15 +139,11 @@ LOGIN_URL = '/accounts/login/'
 # Custome Postgres Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-} 
+}
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
